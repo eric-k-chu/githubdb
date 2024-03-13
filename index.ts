e42a4b9d5e283e79e1ff2a4596b93ea8c06ba40a
Eric Chu <Kiyuen88ec@gmail.com>
2024-03-12 17:40:53 -0700

    Testing pull from repo

---
+++ index.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+
+console.log("Hello World");
+
+async function getRepo() {
+  const username = process.env.GITHUB_USERNAME;
+  if (!username)
+    throw new Error("need to add GITHUB_USERNAME in .env.local file.");
+
+  const res = await fetch(
+    `https://api.github.com/users/${process.env.GITHUB_USERNAME}/repos`
+  );
+  const data = await res.json();
+  console.log(data);
+}
+
+try {
+  getRepo();
+} catch (e) {
+  console.error(e);
+  process.exit(-1);
+}
