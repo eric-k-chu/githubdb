414f96ad173cc5c8e67d6dfcaa9681993736faa5
Eric Chu <Kiyuen88ec@gmail.com>
2024-03-12 17:57:57 -0700

    updated .env to include github pat

--- index.ts
+++ index.ts
@@ -6,9 +6,14 @@ async function getRepo() {
   const username = process.env.GITHUB_USERNAME;
   if (!username) throw new Error("need to add GITHUB_USERNAME in .env file.");
 
-  const res = await fetch(
-    `https://api.github.com/users/${process.env.GITHUB_USERNAME}/repos`
-  );
+  const pat = process.env.GITHUB_PAT;
+  if (!pat) throw new Error("need to add GITHUB_PAT in .env file");
+
+  const res = await fetch(`https://api.github.com/users/${username}/repos`, {
+    headers: {
+      authorization: `token ${pat}`,
+    },
+  });
   const data = await res.json();
   console.log(data);
 }
