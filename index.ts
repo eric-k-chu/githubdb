ed84c0da730e835fdde76d843705c548baaa12d8
Eric Chu <Kiyuen88ec@gmail.com>
2024-03-12 19:42:35 -0700

    added updateJson function

--- index.ts
+++ index.ts
@@ -18,13 +18,43 @@ async function getSHA(): Promise<string> {
       },
     }
   );
+  if (!res.ok) throw new Error(`${res.status}: An error has occured.`);
   const data = (await res.json()) as GitFile;
   return data.sha;
 }
 
+async function updateJson(sha: string) {
+  const username = process.env.GITHUB_USERNAME;
+  if (!username) throw new Error("need to add GITHUB_USERNAME in .env file.");
+
+  const pat = process.env.GITHUB_PAT;
+  if (!pat) throw new Error("need to add GITHUB_PAT in .env file");
+
+  const content = {
+    greeting: "Hello World",
+  };
+
+  const data = {
+    message: "Testing update Data through Node js",
+    content: btoa(JSON.stringify(content)),
+  };
+
+  const res = await fetch(``, {
+    method: "PUT",
+    headers: {
+      authorization: `token ${pat}`,
+      Accept: "application/vnd.github+json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error(`${res.status}: An error has occured.`);
+}
+
 try {
-  const sha = getSHA();
-  console.log(sha);
+  const sha = await getSHA();
+  await updateJson(sha);
 } catch (e) {
   console.error(e);
   process.exit(-1);
